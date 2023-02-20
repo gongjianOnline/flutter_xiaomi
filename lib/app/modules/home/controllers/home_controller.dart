373f61436bb80dbdabbff27c1286ec124ad4041a
gongjianonline <15031617367@163.com>
2023-02-20 21:14:18 +0800

    完成轮播组件和监听滚动条动态切换顶部栏

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -5,14 +5,15 @@ import 'package:bruno/bruno.dart';
 class HomeController extends GetxController {
   // 轮播列表
   RxList imageList = [
-    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ec791…10d7c7541f913759.jpg?f=webp&w=1080&h=540&bg=50912",
-    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d869a…fba61c29b9970a0.jpg?f=webp&w=1080&h=540&bg=8D0509"
-    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/911df…445d49a55263ff7.jpg?f=webp&w=1080&h=540&bg=F5EEE6"
+    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d869ac1e7d35b02defba61c29b9970a0.jpg?f=webp&w=1080&h=540&bg=8D0509",
+    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/d0afa80f550d8071840da4ca1b399088.png?f=webp&w=1080&h=540&bg=0",
+    "https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/ec79106bd79389bf10d7c7541f913759.jpg?f=webp&w=1080&h=540&bg=50912"
   ].obs;
 
   //滚动条高度监听
   ScrollController scrollController = ScrollController();
-
+  //判断顶部栏变化
+  RxBool scrollTopClass = false.obs;
 
 
   
@@ -23,6 +24,18 @@ class HomeController extends GetxController {
     scrollController.addListener(() {
       print("object");
       print(scrollController.position.pixels);
+      if(scrollController.position.pixels > 30){
+        if(!scrollTopClass.value){
+          scrollTopClass.value = true;
+          update();
+        }
+      }else{
+        if(scrollTopClass.value){
+          scrollTopClass.value = false;
+          update();
+        }
+      }
+      print(scrollTopClass.value);
     });
   }
 
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,10 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+
+import 'package:flutter_swiper_3/flutter_swiper_3.dart';
 
 import '../../../../utils/fontIcon.dart';
 import '../controllers/home_controller.dart';
@@ -30,8 +34,17 @@ class HomeView extends GetView<HomeController> {
                   decoration:const BoxDecoration(
                     color: Colors.red
                   ),
+                  child:Swiper(
+                    itemBuilder: (BuildContext context,int index){
+                      return Image.network("${controller.imageList[index]}",fit: BoxFit.cover,);
+                    },
+                    index:0,
+                    itemCount: controller.imageList.length,
+                    pagination: const SwiperPagination(),
+                    control: const SwiperControl(),
+                  ),
                 ),
-                ListTile(title:Text("金山词霸")),
+                ListTile(title:Text("金山词霸${controller.imageList.length}")),
                 ListTile(title:Text("金山词霸")),
                 ListTile(title:Text("金山词霸")),
                 ListTile(title:Text("金山词霸")),
@@ -56,59 +69,67 @@ class HomeView extends GetView<HomeController> {
       
     );
   }
+
+
+  // 顶部导航栏
+  Widget topBar(){
+    return Positioned(
+        top: 0,
+        left: 0,
+        right: 0,
+        child:Obx(()=>Container(
+          width:MyScreenUtil.getScreenWidth(),
+          height: MyScreenUtil.height(98) + MyScreenUtil.getStatusBarHeight()+10,
+          padding: EdgeInsets.only(bottom: 5),
+          decoration:BoxDecoration(
+            color:controller.scrollTopClass.value?Colors.white:const Color.fromRGBO(255, 255, 255, 0)
+          ),
+          child: Row(
+            mainAxisAlignment:MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Icon(MyIcons.xiaomi,color:controller.scrollTopClass.value? Colors.black45 : Colors.white),
+              Container(
+                width: MyScreenUtil.width(648),
+                height: MyScreenUtil.height(97),
+                alignment:Alignment.centerLeft,
+                padding: const EdgeInsets.only(left:10,right:10),
+                decoration: BoxDecoration(
+                  color: controller.scrollTopClass.value?Color.fromRGBO(210, 210, 210,0.4):Color.fromRGBO(222, 221, 221, 0.4),
+                  borderRadius: BorderRadius.circular(MyScreenUtil.radius(97))
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                      child:Row(
+                        children:const  [
+                          Icon(Icons.search,color:Colors.black45),
+                          Text("手机",style: TextStyle(color:Colors.black45))
+                        ],
+                      )
+                    ),
+                    Icon(MyIcons.saomiao,color: Colors.black45,size:MyScreenUtil.fontSize(48))
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: MyScreenUtil.width(187),
+                height: MyScreenUtil.height(97),
+                child: Row(
+                  mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                  children:  [
+                    Icon(MyIcons.code,color: controller.scrollTopClass.value? Colors.black45 : Colors.white),
+                    Icon(MyIcons.xinxi,color: controller.scrollTopClass.value? Colors.black45 : Colors.white),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ))
+      );
+    } 
+
 }
 
 
-// 顶部导航栏
-Widget topBar(){
-  return Positioned(
-          top: MyScreenUtil.getStatusBarHeight(),
-          left: 0,
-          right: 0,
-          child:SizedBox(
-            width:MyScreenUtil.getScreenWidth(),
-            height: MyScreenUtil.height(98),
-            child: Row(
-              mainAxisAlignment:MainAxisAlignment.spaceAround,
-              children: [
-                const Icon(MyIcons.xiaomi,color:Colors.white),
-                Container(
-                  width: MyScreenUtil.width(648),
-                  height: MyScreenUtil.height(97),
-                  alignment:Alignment.centerLeft,
-                  padding: const EdgeInsets.only(left:10,right:10),
-                  decoration: BoxDecoration(
-                    color: const Color.fromRGBO(255, 255, 255, 0.5),
-                    borderRadius: BorderRadius.circular(MyScreenUtil.radius(97))
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      SizedBox(
-                        child:Row(
-                          children:const  [
-                            Icon(Icons.search,color:Colors.black45),
-                            Text("手机",style: TextStyle(color:Colors.black45))
-                          ],
-                        )
-                      ),
-                      Icon(MyIcons.saomiao,color: Colors.black45,size:MyScreenUtil.fontSize(48))
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  width: MyScreenUtil.width(187),
-                  height: MyScreenUtil.height(97),
-                  child: Row(
-                    mainAxisAlignment:MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Icon(MyIcons.code,color:Colors.white),
-                      Icon(MyIcons.xinxi,color:Colors.white),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          )
-        );
-} 
